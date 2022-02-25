128a8feec2fa17528f1cb6280c05532e596a6edf
ghost <jhonsnow751@gmail.com>
2022-02-25 22:47:24 +0100

    jbjnniijooo

--- src/pages/admin/allusers.js
+++ src/pages/admin/allusers.js
@@ -10,17 +10,6 @@ import { getallusers } from "../../config/services";
 import Button from "@mui/material/Button";
 import { navigate } from "@reach/router";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 
 export default function AllUserTablesmain() {
   const [users, setUsers] = React.useState([]);
@@ -38,6 +27,7 @@ export default function AllUserTablesmain() {
           <TableRow>
             <TableCell align="left">Action</TableCell>
             <TableCell align="left">Email</TableCell>
+            <TableCell align="left">Password</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -47,14 +37,13 @@ export default function AllUserTablesmain() {
                 <Button
                   variant="contained"
                   disableElevation
-                  onClick={() =>
-                    navigate(`manager/user/${row.uid}`)
-                  }
+                  onClick={() => navigate(`manager/user/${row.uid}`)}
                 >
                   Edit
                 </Button>
               </TableCell>
               <TableCell align="left">{row.email}</TableCell>
+              <TableCell align="left">{row.password}</TableCell>
             </TableRow>
           ))}
         </TableBody>
